bb9cf1c563cb45505d3b78ef56760877272fb3ae
sclarerot <sclarerot@proton.me>
2025-09-01 12:30:30 -0400

    Add functions to add items, delete items, and show list
    I just wrote the simple functions which will be called when the user
    states which action they'd like to perform.

---
+++ main.py
@@ -0,0 +1,28 @@
+todo_list: list[str] = []
+
+def add_item() -> None:
+    item_to_add: str = input("Enter the task you would like to add: ")
+    item: str = str(len(todo_list)) + " - " + item_to_add
+    todo_list.append(item)
+
+def delete_item() -> None:
+    active: bool = True
+    length_of_list: int = len(todo_list)
+
+    while (active == True):
+        item_to_delete: int = int(input("Please enter the index of the item you would like to delete: "))
+        try:
+            todo_list.pop(item_to_delete)
+            active = False
+        except IndexError:
+            print("Whoops! Couldn't find that item.")
+
+def show_list() -> None:
+    print(todo_list)
+
+def main() -> None:
+
+
+
+if __name__ == "__main__":
+    main()
